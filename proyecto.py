03847171a1ad5770e02b925ee3703b521d5802cd
Romina136 <rominacruz136@gmail.com>
2025-05-06 19:56:00 +0000

    Implemente la funcion 2 y deje las otras para que las completen mis otras compañeras

---
+++ proyecto.py
@@ -0,0 +1,21 @@
+import datetime
+def mostrar_fecha():
+    hoy = datetime.datetime.now()
+    print("Fecha y hora actual:", hoy.strftime("%Y-%m-%d %H:%M:%S"))
+
+def menu():
+    while True:
+        print("\nMenú de opciones:")
+        print("1. Saludar")
+        print("2. Mostrar fecha")
+        print("3. Salir")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "2":
+            mostrar_fecha()
+        else:
+            print("Opción no implementada en esta rama.")
+
+if __name__ == "__main__":
+    menu()
