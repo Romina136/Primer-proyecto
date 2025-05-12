83b01f1469a7832729ae90fd65043db65a3005ad
Aruchi37 <amadaaracelicamachohuanca@gmail.com>
2025-05-12 17:33:52 +0000

    codifique la opcion 3.

---
+++ Primer-Proyecto.py
@@ -0,0 +1,17 @@
+
+def menu():
+    while True:
+        print("\nMenú de opciones:")
+        print("1. Saludar")
+        print("2. Mostrar fecha")
+        print("3. Salir")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "3":
+            print("salir del programa")
+        else:
+            print("Opción no implementada en esta rama.")
+
+if __name__ == "__main__":
+    menu()
